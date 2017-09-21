247616cae57817543a22cfcd057568f186393bce
raymondproduction@ukr.net <raymondproduction@ukr.net>
2017-09-21 18:58:44 +0300

    Added polygon

--- js/script.js
+++ js/script.js
@@ -4,28 +4,19 @@
         var directionsDisplay = new google.maps.DirectionsRenderer({
            draggable: true // можно перетягивать маршрут
         });
+
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 7,
          // center: {lat: 50.35, lng: 30.95} <- Ukraine
           center: chicago
         });
+
+
         directionsDisplay.setMap(map);
 
-        var onChangeHandler = function() {
-          calculateAndDisplayRoute(directionsService, directionsDisplay);
-        };
-        document.getElementById('start').addEventListener('change', onChangeHandler);
-        document.getElementById('end').addEventListener('change', onChangeHandler);
-        document.getElementById('way').addEventListener('change', onChangeHandler);
-        directionsDisplay.addListener('directions_changed', function(directionsService) {
-          console.log('dragg: ',showLatLng(directionsDisplay.getDirections()));
-        });
-      }
-
-      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
         directionsService.route({
-          origin: document.getElementById('start').value,
-          destination: document.getElementById('end').value,
+          origin: {lat: 41.850033, lng: -87.6500523} ,
+          destination:  {lat: 39.974611666223815,  lng: -92.968640612},
           // waypoints: [
           //   {
           //     location: 'Joplin, MO',
@@ -34,6 +25,69 @@
           //     location: 'Oklahoma City, OK',
           //     stopover: true
           //   }],
+          waypoints: [
+            {
+              location:  {lat :39.68072,  lng: -90.39102000000001},
+              stopover: true
+            }],
+          travelMode: 'DRIVING'}
+        , function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+            showLatLng(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+
+        var onChangeHandler = function() {
+          calculateAndDisplayRoute(directionsService, directionsDisplay);
+        };
+
+        var onShowPolygon = function() {
+          console.log('OK ',this.checked);
+          if (this.checked){
+            path=getCoordsDirections(directionsDisplay);
+            polyline = new google.maps.Polyline({
+              path: path,
+              geodesic: true,
+              strokeColor: '#AAAAFF',
+              strokeOpacity: 0.9,
+              draggable: true,
+              editable: true,
+              clickable:  true,
+              strokeWeight: 4
+            });
+            polyline.addListener('polyline_changed',()=>console.log('YES'));
+            console.log(path);
+            polyline.setMap(map);
+          } else {
+            polyline.setMap(null);
+          }
+        };
+    //   document.getElementById('start').addEventListener('change', onChangeHandler);
+    //   document.getElementById('end').addEventListener('change', onChangeHandler);
+    //   document.getElementById('way').addEventListener('change', onChangeHandler);
+        document.getElementById('polyline').addEventListener('change', onShowPolygon);
+        directionsDisplay.addListener('directions_changed', function(directionsService) {
+          console.log('dragg: ',showLatLng(directionsDisplay.getDirections()));
+          if (document.getElementById('polyline').checked){
+           // polyline.setMap(null);
+            polyline.setPath(getCoordsDirections(directionsDisplay));
+           // polyline.setMap(map);
+          }
+        });
+        // google.maps.event.addListener(polyline, 'click', function()
+        // {
+        //     console.log('YES')
+        // });
+
+      }
+
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        directionsService.route({
+          origin: document.getElementById('start').value,
+          destination: document.getElementById('end').value,
           waypoints: [
            {
              location: document.getElementById('way').value,
@@ -53,17 +107,40 @@
 
 
       function showLatLng(directionsResult){
+        function getLat(point){
+          return point.location ? getLat(point.location) : point.lat();
+        };
+        function getLng(point){
+          return point.location ? getLng(point.location) : point.lng();
+        };
          console.log(directionsResult);
         if (directionsResult.request.origin.query===undefined)
-          console.log('start',directionsResult.request.origin.lat(),' ',directionsResult.request.origin.lng());
+          console.log('start',getLat(directionsResult.request.origin),' ',getLng(directionsResult.request.origin));
         if (directionsResult.request.destination.query===undefined)
-          console.log('end',directionsResult.request.destination.lat(),' ',directionsResult.request.destination.lng());
+          console.log('end',getLat(directionsResult.request.destination),' ',getLng(directionsResult.request.destination));
         directionsResult.request.waypoints.forEach((point) => {
         if (point.location.query===undefined)
-             console.log('waypoint: ',point.location.lat(),' ',point.location.lat());
+             console.log('waypoint: ',getLat(point),' ',getLng(point));
         })
         console.log('Geo coordinates:');
         directionsResult.routes[0].overview_path.forEach((point) => {
-              console.log('latitude',point.lat(),' longitude',point.lng());
+              console.log('latitude',getLat(point),' longitude',getLng(point));
         });
+      }
+
+      function getCoordsDirections(directionsDisplay){
+        function getLat(point){
+          return point.location ? getLat(point.location) : point.lat();
+       };
+        function getLng(point){
+          return point.location ? getLng(point.location) : point.lng();
+        };
+        let path = [];
+        let response =  directionsDisplay.getDirections();
+        if (!response) return;
+        console.log('Response directionsDisplay >>>>',response.request);
+        path.push({lat : getLat(response.request.origin), lng : getLng(response.request.origin)});
+        response.request.waypoints.forEach((point)=> path.push({lat : getLat(point), lng : getLng(point)})),
+        path.push({lat : getLat(response.request.destination), lng : getLng(response.request.destination)});
+        return path;
       }
